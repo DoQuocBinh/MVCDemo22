58102f4e54d1e3370da9af35100e36a10119580d
DoQuocBinh <binhdq@fpt.edu.vn>
2024-03-22 10:14:12 +0700

    ok done

---
+++ MVCDemo2/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCDemo2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<CustomerEntity> registeredCustomers
+            = new List<CustomerEntity>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Register(CustomerEntity customerEntity)
+        {
+            registeredCustomers.Add(customerEntity);
+            return View("ListCustomers", registeredCustomers);
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ MVCDemo2/Models/CustomerEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MVCDemo2.Models
+{
+	public class CustomerEntity
+	{
+		public String Name { get; set; }
+		public int Age { get; set; }
+		public bool isAcceptingNews { get; set; }
+		public CustomerEntity()
+		{
+		}
+	}
+}
+
